03297afa821a6bd3f3bc3b71bc0ff8d9b2774d35
Kae <kaehehehe@gmail.com>
2022-04-01 00:50:51 +0900

    :lipstick: hover가 되었을 때 배경색이 바뀌도록 구현

--- src/components/Keyword/style.js
+++ src/components/Keyword/style.js
@@ -5,6 +5,11 @@ export const Item = styled.li`
   align-items: center;
   font-size: 1.5rem;
   padding: 15px 0;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.12);
+    cursor: pointer;
+  }
 `;
 
 export const Name = styled.span`
